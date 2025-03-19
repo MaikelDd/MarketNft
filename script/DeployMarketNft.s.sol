57ccb80dbad224a8e42fb78f856b0de2b2ea5a71
maikel <maikel@LAPTOP-H89CHH9V.>
2025-03-19 09:43:39 +0100

    Added functions to add new Nft

--- script/DeployMarketNft.s.sol
+++ script/DeployMarketNft.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {MarketNft} from "../src/MarketNft.sol";
+import {Base64} from "lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract DeployMarketNft is Script {
     function run() external returns (MarketNft) {
